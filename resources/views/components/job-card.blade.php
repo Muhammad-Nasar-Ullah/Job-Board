5092631842102a371c2613cab1223dce1ef3ac81
Muhammad-Nasar-Ullah <167764303+Muhammad-Nasar-Ullah@users.noreply.github.com>
2024-07-11 18:22:55 +0500

    made a radio group to shorten the list according to need

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -28,11 +28,6 @@
                 {{ $job->category }}
             </div>
         </div>
-
-    </div>
-
-    <div class="text-sm mb-4 text-slate-500">
-        {!! nl2br(e($job->description)) !!}
     </div>
 
     {{ $slot }}
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -3,5 +3,9 @@
     <x-breadcrumbs class="mb-4"
     :links="['jobs' => route('jobs.index'), $job->title => '#']"    />
 
-    <x-job-card :$job />
+    <x-job-card :$job>
+        <div class="text-sm mb-4 text-slate-500">
+            {!! nl2br(e($job->description)) !!}
+        </div>
+    </x-job-card>
 </x-layout>
